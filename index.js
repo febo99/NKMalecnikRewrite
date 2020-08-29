72915dd50c9d6bf4e40a4451375f3024cc798a97
Žiga Lah <ziga.lah999@gmail.com>
2020-08-29 09:39:10 +0200

    * Added morgan for loggin * Basic post route * User controller * Basic navbar pug

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import mysql from 'mysql';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import path from 'path';
+import fs from 'fs';
+import bodyParser from 'body-parser';
 
-const path = require('path');
+// Routers definition
+import indexRouter from './routes/index';
+import userRouter from './routes/userRouter';
 
 const app = express();
 const port = 3000;
+
+const logger = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
 
 dotenv.config(); // reads variables from .env
 
@@ -13,6 +21,11 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(`${__dirname}/public`));
+
+app.use(morgan('combined', { stream: logger }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Database connection
 app.use((req, res, next) => {
@@ -26,11 +39,9 @@ app.use((req, res, next) => {
   next();
 });
 
-// Routers definition
-const indexRouter = require('./routes/index');
-
 // Using routers
 app.use('/', indexRouter);
+app.use('/login', userRouter);
 
 app.listen(port, () => {
   console.log(`Started a server at http://localhost:${port}`);
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.locals.connection.query('SELECT * FROM users', (err, rows) => {
-    if (err) throw err;
-    console.log(rows);
-  });
-  res.render('index', { title: 'Express', message: 'Testiram' });
+  res.render('index');
 });
 
 module.exports = router;
