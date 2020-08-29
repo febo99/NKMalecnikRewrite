fd121bcfd3325e7a4da8ebc27e448657547309b6
Žiga Lah <ziga.lah999@gmail.com>
2020-08-29 08:30:47 +0200

    * Basic database connection * Removed no-console warning

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from 'express';
+import mysql from 'mysql';
+import dotenv from 'dotenv';
 
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
+dotenv.config(); // reads variables from .env
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(`${__dirname}/public`));
+
+// Database connection
+app.use((req, res, next) => {
+  res.locals.connection = mysql.createConnection({
+    host: process.env.DATABASE_IP,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DB_NAME,
+  });
+  res.locals.connection.connect();
+  next();
+});
 
 // Routers definition
 const indexRouter = require('./routes/index');
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
+  res.locals.connection.query('SELECT * FROM users', (err, rows) => {
+    if (err) throw err;
+    console.log(rows);
+  });
   res.render('index', { title: 'Express', message: 'Testiram' });
 });
 
