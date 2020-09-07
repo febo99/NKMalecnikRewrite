34188475748306fdb3710e435729ff5300dd2c34
Žiga Lah <ziga.lah999@gmail.com>
2020-09-07 16:35:37 +0200

    * Location adding, editting and displaying

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -17,6 +17,17 @@ module.exports = {
     }
   },
 
+  location: (req, res) => {
+    if (userLogged(req)) {
+      res.locals.connection.query('SELECT * FROM locations WHERE ID = ?', req.params.id, (err, rows) => {
+        if (err) return res.json({ err });
+        return res.render('settings/location', { location: rows[0] });
+      });
+    } else {
+      res.redirect('/');
+    }
+  },
+
   addLocationForm: (req, res) => {
     if (userLogged(req)) {
       const err = req.session.error;
@@ -32,10 +43,30 @@ module.exports = {
       res.locals.connection.query('SELECT * FROM locations WHERE name = ?', newLocation.name, (err, rows) => {
         if (err) return res.json({ err });
         if (rows.length > 0) { // check if location with same name already exsits
-          res.session.error = 'Lokacija s tem imenom ze obstaja!';
+          req.session.error = 'Lokacija s tem imenom ze obstaja!';
           return res.redirect('/settings/add-location');
         }
         return res.locals.connection.query('INSERT INTO locations VALUES ?', [[newLocation.parseInsert()]], (err1, result) => {
+          if (err1) return res.json({ err1 });
+          return res.json({ result });
+        });
+      });
+    } else {
+      res.redirect('/');
+    }
+  },
+
+  editLocation: (req, res) => {
+    const newLocation = new Location(req.body.name, req.body.color);
+    const id = Number.parseInt(req.params.id, 10);
+    if (userLogged(req)) {
+      res.locals.connection.query('SELECT * FROM locations WHERE name = ?', newLocation.name, (err, rows) => {
+        if (err) return res.json({ err });
+        if (rows.length > 0) { // check if location with same name already exsits
+          req.session.error = 'Lokacija s tem imenom ze obstaja!';
+          return res.redirect('/settings/add-location');
+        }
+        return res.locals.connection.query('UPDATE locations SET name = ?, color = ? WHERE ID = ? ', [newLocation.name, newLocation.color, id], (err1, result) => {
           if (err1) return res.json({ err1 });
           return res.json({ result });
         });
--- routes/settingsRouter.js
+++ routes/settingsRouter.js
@@ -6,7 +6,9 @@ const router = express.Router();
 router.get('/', settingsController.settings);
 router.get('/new-location', settingsController.addLocationForm);
 router.get('/locations', settingsController.locations);
+router.get('/locations/edit-location/:id', settingsController.location);
 
 router.post('/addLocation', settingsController.addLocation);
+router.post('/editLocation/:id', settingsController.editLocation);
 
 module.exports = router;
