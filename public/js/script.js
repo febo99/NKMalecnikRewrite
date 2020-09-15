2748abfb1adc70ac68b98d9b731e2c1d1a79eff8
Žiga Lah <ziga.lah999@gmail.com>
2020-09-15 19:18:59 +0200

    * Added script for max char length of input

--- public/js/script.js
+++ public/js/script.js
@@ -2,15 +2,20 @@ const updatePostCharCounter = () => {
   const charLength = document.querySelector('#post').value.length;
   document.querySelector('#charCounter').innerHTML = `${charLength}/250`;
   if (charLength >= 250) return false;
+  return true;
 };
 
 const updateCommentCharCounter = (id) => {
-
+  const charLength = document.querySelector(`#comment${id}`).value.length;
+  document.querySelector(`#commentCounter${id}`).innerHTML = `${charLength}/250`;
+  if (charLength >= 250) return false;
+  return true;
 };
 
 window.addEventListener('load', () => {
+  console.log('test');
   document.querySelector('#post').addEventListener('input', updatePostCharCounter);
-
+  updateCommentCharCounter(0);
   const commentCounter = document.querySelectorAll('.commentCounter');
   commentCounter.forEach((item) => {
     item.addEventListener('input');
