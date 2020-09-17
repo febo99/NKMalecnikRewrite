6387cb3db25672d642f338cf1c7dded26aed5ad3
Žiga Lah <ziga.lah999@gmail.com>
2020-09-17 16:41:13 +0200

    * Specific team page * Editing and deleting team only by admin

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -4,12 +4,85 @@ import Team from '../models/teamModel';
 module.exports = {
   getAllTeams: (req, res) => {
     if (userLogged(req)) {
-      res.locals.connection.query('SELECT *,users.name AS username, teams.name AS team FROM teams INNER JOIN users on teams.coachID = users.ID', (err, rows) => {
+      const query = 'SELECT *,users.name AS username, teams.name AS team,teams.ID AS teamID FROM teams INNER JOIN users on teams.coachID = users.ID';
+      res.locals.connection.query(query, (err, rows) => {
         if (err) {
           res.json({ error: err });
           throw err;
         }
         return res.render('teams/teams', { teams: rows });
+      });
+    } else {
+      res.redirect('/');
+    }
+  },
+
+  getTeam: (req, res) => {
+    const teamID = req.params.id;
+    if (userLogged(req)) {
+      const query = 'SELECT *,users.name AS userName, teams.name AS team, teams.ID AS teamID FROM teams INNER JOIN users on teams.coachID = users.ID WHERE teams.ID = ?';
+      res.locals.connection.query(query, teamID, (err, team) => {
+        if (err) {
+          req.session.error = `Napaka pri pridobivanju te ekipe! Koda napake ${err} `;
+          res.redirect('/teams');
+        }
+        const queryPlayers = 'SELECT * FROM players WHERE teamID = ?';
+        res.locals.connection.query(queryPlayers, teamID, (err2, players) => {
+          if (err2) {
+            req.session.error = `Napaka pri pridobivanju te ekipe! Koda napake ${err} `;
+            res.redirect('/teams');
+          }
+          res.render('teams/team', {
+            user: {
+              email: req.session.email,
+              role: req.session.role,
+              id: req.session.userID,
+              name: req.session.name,
+              surname: req.session.surname,
+            },
+            team: team[0],
+            players,
+
+          });
+        });
+      });
+    } else {
+      res.redirect('/');
+    }
+  },
+
+  editTeamForm: (req, res) => {
+    const teamID = req.params.id;
+    const { error } = req.session;
+    req.session.error = null;
+    if (userLogged(req)) {
+      const query = 'SELECT *,users.name AS userName, teams.ID as teamID, teams.name AS team FROM teams INNER JOIN users on teams.coachID = users.ID WHERE teams.ID = ?';
+      res.locals.connection.query(query, teamID, (err, team) => {
+        if (err) {
+          req.session.error = `Napaka pri pridobivanju te ekipe! Koda napake ${err} `;
+          res.redirect('/teams');
+        }
+        // eslint-disable-next-line no-param-reassign
+        if (team[0].notes === null) team[0].notes = '';
+        const queryUsers = 'SELECT * FROM users';
+        res.locals.connection.query(queryUsers, (err2, users) => {
+          if (err2) {
+            req.session.error = `Napaka pri pridobivanju te ekipe! Koda napake ${err2} `;
+            res.redirect('/teams');
+          }
+          res.render('teams/editTeam', {
+            user: {
+              email: req.session.email,
+              role: req.session.role,
+              id: req.session.userID,
+              name: req.session.name,
+              surname: req.session.surname,
+            },
+            team: team[0],
+            users,
+            error,
+          });
+        });
       });
     } else {
       res.redirect('/');
@@ -33,7 +106,7 @@ module.exports = {
   },
 
   addTeam: (req, res) => {
-    if (userLogged(req)) {
+    if (userLogged(req) && (req.session.role === 2 || req.session.role === 1)) {
       const error = {};
       const newTeam = new Team(req.body.name, req.body.notes, req.body.coach,
         req.body.assistant, req.body.technical);
@@ -52,7 +125,56 @@ module.exports = {
         });
       });
     } else {
-      res.redirect('/');
+      req.session.error = 'Nimas pravice za to operacijo!';
+      return res.redirect('/teams/add-team');
+    }
+  },
+
+  editTeam: (req, res) => {
+    if (userLogged(req) && (req.session.role === 2 || req.session.role === 1)) {
+      const teamID = Number.parseInt(req.params.id, 10);
+      const editTeam = new Team(req.body.name, req.body.note, req.body.coach,
+        req.body.assistant, req.body.technical);
+
+      const query = 'UPDATE teams SET name = ?, notes = ?, coachID = ?, assistantID = ?, technicalID = ? WHERE ID = ?';
+      res.locals.connection.query(query, [editTeam.name, editTeam.notes,
+        editTeam.coachID, editTeam.assistantID, editTeam.technicalID, teamID],
+      (err) => {
+        if (err) {
+          req.session.error = `Napaka pri urejanju ekipe! Koda napake ${err}`;
+          return res.redirect(`/teams/edit-team/${teamID}`);
+        }
+        req.session.error = 'Uspesno urejanje ekipe!';
+        return res.redirect(`/teams/edit-team/${teamID}`);
+      });
+    } else {
+      req.session.error = 'Nimas pravice za to operacijo!';
+      return res.redirect('/teams');
+    }
+  },
+
+  deleteTeam: (req, res) => {
+    if (userLogged(req) && (req.session.role === 2 || req.session.role === 1)) {
+      const teamID = Number.parseInt(req.params.id, 10);
+      const playerQuery = 'UPDATE players SET teamID = 0 WHERE teamID = ?';
+      res.locals.connection.query(playerQuery, teamID, (err) => {
+        if (err) {
+          req.session.error = `Napaka pri brisanju ekipe! Koda napake ${err}`;
+          return res.redirect(`/teams/edit-team/${teamID}`);
+        }
+        const query = 'DELETE FROM teams WHERE ID = ?';
+        res.locals.connection.query(query, teamID, (err1) => {
+          if (err1) {
+            req.session.error = `Napaka pri brisanju ekipe! Koda napake ${err1}`;
+            return res.redirect(`/teams/edit-team/${teamID}`);
+          }
+          req.session.error = 'Uspesno izbrisana ekipa!';
+          return res.redirect('/teams');
+        });
+      });
+    } else {
+      req.session.error = 'Nimas pravice za to operacijo!';
+      return res.redirect('/teams');
     }
   },
 };
--- routes/teamsRouter.js
+++ routes/teamsRouter.js
@@ -5,7 +5,11 @@ const router = express.Router();
 
 router.get('/', teamsController.getAllTeams);
 router.get('/add-team', teamsController.addTeamForm);
+router.get('/team/:id', teamsController.getTeam);
+router.get('/edit-team/:id', teamsController.editTeamForm);
 
 router.post('/addTeam', teamsController.addTeam);
+router.post('/editTeam/:id', teamsController.editTeam);
+router.post('/deleteTeam/:id', teamsController.deleteTeam);
 
 module.exports = router;
