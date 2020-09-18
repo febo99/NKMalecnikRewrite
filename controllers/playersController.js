b71c43e5d42b1eb9aa7a92a2e6f061edf232b0f8
Žiga Lah <ziga.lah999@gmail.com>
2020-09-18 16:42:53 +0200

    * Added page for player * Added editing page * Endpoint for editing player * Added new date formatters to util * Minor tweaks to navbar

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -1,5 +1,6 @@
 import Player from '../models/playerModel';
 import { userLogged } from '../utils/checkLogin';
+import { formatDate, htmlInputFormatDate } from '../utils/formatDate';
 
 module.exports = {
   getAllPlayers: (req, res) => {
@@ -11,8 +12,7 @@ module.exports = {
         }
         rows.forEach((row) => {
           const player = row;
-          const date = new Date(row.dateOfBirth);
-          player.dateOfBirth = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+          player.dateOfBirth = formatDate(row.dateOfBirth);
         });
         return res.render('players/players', {
           players: rows,
@@ -38,8 +38,7 @@ module.exports = {
         }
         rows.forEach((row) => {
           const player = row;
-          const date = new Date(row.dateOfBirth);
-          player.dateOfBirth = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+          player.dateOfBirth = formatDate(row.dateOfBirth);
         });
         return res.render('players/myPlayers', {
           players: rows,
@@ -50,6 +49,45 @@ module.exports = {
             name: req.session.name,
             surname: req.session.surname,
           },
+        });
+      });
+    } else {
+      res.redirect('/');
+    }
+  },
+
+  getPlayer: (req, res) => {
+    if (userLogged(req)) {
+      const playerID = req.params.id;
+      res.locals.connection.query('SELECT * FROM players WHERE ID = ? ', playerID, (err, player) => {
+        res.locals.connection.query('SELECT * FROM teams ', (err1, teams) => {
+          if (err1) {
+            req.session.error = `Napaka pri pridobivanju podatkov! Koda napake: ${err1}`;
+            res.redirect('/players');
+          }
+          res.locals.connection.query('SELECT * FROM goalkeeperTeams ', (err2, gkTeams) => {
+            if (err2) {
+              req.session.error = `Napaka pri pridobivanju podatkov! Koda napake: ${err1}`;
+              res.redirect('/players');
+            }
+
+            const playerParam = player;
+            if (playerParam[0].note === null) playerParam[0].note = '';
+            playerParam[0].dateOfBirth = formatDate(playerParam[0].dateOfBirth);
+
+            res.render('players/player', {
+              user: {
+                email: req.session.email,
+                role: req.session.role,
+                id: req.session.userID,
+                name: req.session.name,
+                surname: req.session.surname,
+              },
+              player: playerParam[0],
+              teams,
+              gkTeams,
+            });
+          });
         });
       });
     } else {
@@ -141,6 +179,8 @@ module.exports = {
 
   editPlayerForm: (req, res) => {
     if (userLogged(req)) {
+      const { error } = req.session;
+      req.session.error = null;
       const playerID = req.params.id;
       res.locals.connection.query('SELECT * FROM players WHERE ID = ? ', playerID, (err, player) => {
         res.locals.connection.query('SELECT * FROM teams ', (err1, teams) => {
@@ -153,8 +193,10 @@ module.exports = {
               req.session.error = `Napaka pri pridobivanju podatkov! Koda napake: ${err1}`;
               res.redirect(`/players/edit-player/${playerID}`);
             }
-            // eslint-disable-next-line no-param-reassign
-            if (player[0].note === null) player[0].note = '';
+            const playerParam = player;
+            if (playerParam[0].note === null) playerParam[0].note = '';
+            playerParam[0].dateOfBirth = htmlInputFormatDate(playerParam[0].dateOfBirth);
+
             res.render('players/editPlayer', {
               user: {
                 email: req.session.email,
@@ -163,9 +205,10 @@ module.exports = {
                 name: req.session.name,
                 surname: req.session.surname,
               },
-              player: player[0],
+              player: playerParam[0],
               teams,
               gkTeams,
+              error,
             });
           });
         });
@@ -177,8 +220,63 @@ module.exports = {
 
   editPlayer: (req, res) => {
     if (userLogged(req)) {
-      return res.json({ test: 'test' });
+      const error = {};
+      const phoneNumberRegex = /^[\d ()+-]+$/;
+      const playerID = Number.parseInt(req.params.id, 10);
+
+      const newPlayer = new Player(req.body.name, req.body.surname, req.body.dateOfBirth,
+        req.body.nationality === 'on' ? 0 : 1, req.body.gkTeam, req.body.address, req.body.postNumber,
+        req.body.postName, req.body.playerPhone, req.body.playerEmail, req.body.dadName,
+        req.body.dadPhone, req.body.dadEmail,
+        req.body.mumName, req.body.mumPhone, req.body.mumEmail,
+        req.body.emso, req.body.registerNumber, req.body.note, req.body.team, req.session.userID);
+
+      // check if all not-null values are filled
+
+      if (newPlayer.name === null || newPlayer.name === '') error.name = 'Obvezno polje - ime';
+      if (newPlayer.surname === null || newPlayer.surname === '') error.surname = 'Obvezno polje - priimek';
+      if (newPlayer.address === null || newPlayer.address === '') error.address = 'Obvezno polje - naslov';
+      if (newPlayer.postNumber === null || newPlayer.postNumber === '') error.postNumber = 'Obvezno polje - postna stevilka';
+      if (newPlayer.postName === null || newPlayer.postName === '') error.postName = 'Obvezno polje - posta';
+      if (newPlayer.team === null || newPlayer.team === '') error.team = 'Obvezno polje - ekipa';
+
+      // values validation TO-DO
+      if (!phoneNumberRegex.test(newPlayer.playerPhone) || newPlayer.playerPhone === '') error.playerPhone = 'Neveljavna telefonska stevilka igralca!';
+      if (!phoneNumberRegex.test(newPlayer.dadPhone) || newPlayer.dadPhone === '') error.dadPhone = 'Neveljavna telefonska stevilka oceta!';
+      if (!phoneNumberRegex.test(newPlayer.mumPhone) || newPlayer.mumPhone === '') error.mumPhone = 'Neveljavna telefonska stevilka mame!';
+      if (newPlayer.emso.length !== 13) error.emso = 'Neveljaven emso!';
+      if (newPlayer.postNumber.length !== 4) error.postNumber = 'Neveljavna postna stevilka!';
+      // nationality = 1 => slovenia, others = 0
+
+      if (Object.keys(error).length !== 0) {
+        req.session.error = error;
+        req.session.oldValues = req.body;
+        return res.redirect(`/players/edit-player/${playerID}`);
+      }
+
+      if (Object.keys(error).length === 0) {
+        const query = `UPDATE players SET name = ?, surname = ?, dateOfBirth = ?, nationality = ?, goalkeeperTeamID = ?, address = ?, postNumber =?,
+        post = ?, playerPhone = ?, dadName = ?, dadPhone = ?, dadEmail = ? , mumName = ?, mumPhone = ?, mumEmail = ?, emso = ?, registerNumber = ?,
+        note = ?, teamID = ? WHERE ID = ?`;
+
+        const insertPlayer = newPlayer.parseInsert();
+        insertPlayer.splice(0, 1); // remove first item that equals to null
+        insertPlayer.splice(insertPlayer.length - 1, 1); // remove latest item sessionID
+        insertPlayer.push(playerID); // Add playerID for WHERE clause
+
+        res.locals.connection.query(query, insertPlayer, (err, result) => {
+          console.log(result);
+          if (err) {
+            req.session.error = err;
+            return res.redirect(`/players/edit-player/${playerID}`);
+          }
+          console.log('here2');
+          req.session.error = 'Uspesno urejen igralec!';
+          return res.redirect(`/players/edit-player/${playerID}`);
+        });
+      }
+    } else {
+      return res.redirect('/');
     }
-    return res.redirect('/');
   },
 };
--- routes/playersRouter.js
+++ routes/playersRouter.js
@@ -7,8 +7,9 @@ router.get('/', playersController.getAllPlayers);
 router.get('/my-players', playersController.getMyPlayers);
 router.get('/new-player', playersController.newPlayerForm);
 router.get('/edit-player/:id', playersController.editPlayerForm);
+router.get('/player/:id', playersController.getPlayer);
 
 router.post('/addPlayer', playersController.addPlayer);
-router.post('/editPlayer', playersController.editPlayer);
+router.post('/editPlayer/:id', playersController.editPlayer);
 
 module.exports = router;
