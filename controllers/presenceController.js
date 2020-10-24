a6239ebda7f4104671c88b3b983b5099b618f868
Žiga Lah <ziga.lah999@gmail.com>
2020-10-24 20:37:28 +0200

    * Better match presence form * Presence updates correctly * Fixed minor issues on training presence

--- controllers/presenceController.js
+++ controllers/presenceController.js
@@ -7,7 +7,6 @@ module.exports = {
 
   setTrainingPresence: (req, res) => {
     if (userLogged(req)) {
-      const { error } = req.session;
       req.session.error = null;
 
       let updateQueries = '';
@@ -20,9 +19,9 @@ module.exports = {
         updateQueries += `UPDATE presenceTrainings SET presence = ${presenceStatus} WHERE playerID = ${playerID} AND trainingID = ${trainingID};`;
       });
 
-      res.locals.connection.query(updateQueries, (err, trainings) => {
+      res.locals.connection.query(updateQueries, (err) => {
         if (err) {
-          req.session.error = { error: `Napaka pri pridobivanju treningov! Koda napake: ${err}` };
+          req.session.error = { error: `Napaka pri posodobitvi prisotnosti! Koda napake: ${err}` };
           console.log(err);
           res.redirect('/');
         }
@@ -34,6 +33,38 @@ module.exports = {
   },
 
   setMatchPresence: (req, res) => {
+    if (userLogged(req)) {
+      req.session.error = null;
 
+      let updateQueries = '';
+      const matchID = req.params.id;
+      const values = Object.entries(req.body);
+      for (let i = 0; i < values.length / 5; i += 1) {
+        const playerID = Number.parseInt(values[0 + i * 5][0].replace('presence', ''), 10);
+        const presenceStatus = Number.parseInt(values[0 + i * 5][1], 10);
+        const minutes = Number.parseInt(values[1 + i * 5][1], 10);
+        const goals = Number.parseInt(values[2 + i * 5][1], 10);
+        const asissts = Number.parseInt(values[3 + i * 5][1], 10);
+        const cards = String(values[4 + i * 5][1]);
+
+        updateQueries += `UPDATE presenceMatches SET presence = ${presenceStatus}, 
+        minutes = ${minutes},
+        goals = ${goals},
+        asissts = ${asissts},
+        cards = "${cards}"
+        WHERE playerID = ${playerID} AND matchID = ${matchID};`;
+      }
+
+      res.locals.connection.query(updateQueries, (err) => {
+        if (err) {
+          req.session.error = { error: `Napaka pri posodobitvi prisotnosti! Koda napake: ${err}` };
+          console.log(err);
+          return res.redirect('/');
+        }
+        res.redirect(`/matches/match/${matchID}`);
+      });
+    } else {
+      res.redirect('/');
+    }
   },
 };
