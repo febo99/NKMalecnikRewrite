e3688769454bce3da1816f32b13bfa5c3ad85b4e
Žiga Lah <ziga.lah999@gmail.com>
2020-10-24 19:52:31 +0200

    * Update SQL is working * Better form

--- controllers/presenceController.js
+++ controllers/presenceController.js
@@ -1,10 +1,36 @@
+import { userLogged } from '../utils/checkLogin';
+
 module.exports = {
   presence: (req, res) => {
     res.render('./presence/presence');
   },
 
   setTrainingPresence: (req, res) => {
-    console.log(req.body);
+    if (userLogged(req)) {
+      const { error } = req.session;
+      req.session.error = null;
+
+      let updateQueries = '';
+      const trainingID = req.params.id;
+
+      Object.entries(req.body).forEach((item) => {
+        const playerID = Number.parseInt(item[0].replace('presence', ''), 10);
+        const presenceStatus = Number.parseInt(item[1], 10);
+
+        updateQueries += `UPDATE presenceTrainings SET presence = ${presenceStatus} WHERE playerID = ${playerID} AND trainingID = ${trainingID};`;
+      });
+
+      res.locals.connection.query(updateQueries, (err, trainings) => {
+        if (err) {
+          req.session.error = { error: `Napaka pri pridobivanju treningov! Koda napake: ${err}` };
+          console.log(err);
+          res.redirect('/');
+        }
+        res.redirect(`/trainings/training/${trainingID}`);
+      });
+    } else {
+      res.redirect('/');
+    }
   },
 
   setMatchPresence: (req, res) => {
